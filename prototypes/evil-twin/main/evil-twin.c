5cec46a3e93ac15549f0d9b4c7136ad78a04e03b
danny-turner-git <daniel.edenturner@gmail.com>
2025-04-06 14:16:38 +0100

    Refactor scanner function
    Refactored the AP scanner function to scan for available APs and return
    a struct of the AP records and the number of APs found.

--- prototypes/evil-twin/main/evil-twin.c
+++ prototypes/evil-twin/main/evil-twin.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
@@ -15,19 +16,13 @@
 char * TAG = "debug";
 char * AP_TAG = "AP";
 
-void app_main()
+typedef struct{
+    wifi_ap_record_t *ap_records;
+    uint16_t num_ap;
+} Scan;
+
+Scan scanner()
 {
-    nvs_flash_init(); // wifi configuration stored in nvs
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-
-    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // create default config here and alter config later
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    esp_wifi_set_mode(WIFI_MODE_STA); // station mode for ssid scans
-    ESP_ERROR_CHECK(esp_wifi_start());
-
     wifi_scan_config_t scan_cfg = {
       .ssid = NULL,
       .bssid = NULL,
@@ -44,10 +39,32 @@ void app_main()
     wifi_ap_record_t *ap_records = calloc(num_ap, sizeof(*ap_records));
     ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&num_ap, ap_records));
 
-    int rec = 0;
-    for (rec = 0; rec < num_ap; rec++){ // iterate through each record to output found APs
-        ESP_LOGI(AP_TAG, "%s", ap_records[rec].ssid);
-    };
+    Scan ap_scan;
+    ap_scan.ap_records = ap_records;
+    ap_scan.num_ap = num_ap;
+    return ap_scan;
 
+
+};
+
+void app_main()
+{
+    nvs_flash_init(); // wifi configuration stored in nvs
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+
+    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // create default config here and alter config later
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_wifi_set_mode(WIFI_MODE_APSTA); // STA needed for scanning and AP needed for evil twin
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    Scan ap_scan = scanner();
+
+    int rec = 0;
+    for (rec = 0; rec < ap_scan.num_ap; rec++){ // iterate through each record to output found APs
+        ESP_LOGI(AP_TAG, "%s", ap_scan.ap_records[rec].ssid);
+    };
 
 }
